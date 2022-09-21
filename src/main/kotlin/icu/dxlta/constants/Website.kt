29222884f9b9aaf440eddb8b56516e2ef041870c
gemsvido <gemsvido@gmail.com>
2022-09-21 12:05:24 -0700

    [website] Upgrade the website

--- src/main/kotlin/icu/dxlta/constants/Website.kt
+++ src/main/kotlin/icu/dxlta/constants/Website.kt
@@ -7,9 +7,9 @@ object Website {
 
     /** Insert your own contents of index.html here, if not Nil's normal index.html will be used.
      * CAUTION: Remember that this is the exported main page of the site. Be wise. */
-    val INDEX : String = FileReader("./build/resources/main/index.html").readText()
+    val INDEX : String = FileReader("./public/index.html").readText()
 
     /** Insert your own contents of style.css here, if not Nil's normal style.css will be used.
      * CAUTION: This is exported at `<site>/style.css`. Remember to insert a stylesheet link in index.html to use this. */
-    val STYLE : String = FileReader("./build/resources/main/style.css").readText()
+    val STYLE : String = FileReader("./public/style.css").readText()
 }
