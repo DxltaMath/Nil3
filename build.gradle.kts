64b9311fde4360dc993a22c97cfe70b9505e9dc3
gemsvido <gemsvido@gmail.com>
2022-10-13 10:14:20 -0700

    [jar] Fix JAR compilation

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.7.10"
+    id("io.ktor.plugin") version "2.1.2"
     application
 }
 
@@ -37,22 +38,9 @@ application {
     mainClass.set("icu.dxlta.MainKt")
 }
 
-/*
-tasks {
-    val packJar = register<Jar>("packJar") {
-        dependsOn.addAll(listOf("compileJava", "compileKotlin", "processResources")) // We need this for Gradle optimization to work
-        archiveClassifier.set("standalone") // Naming the jar
-        duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-        manifest { attributes(mapOf("Main-Class" to application.mainClass)) } // Provided we set it up in the application plugin configuration
-        val sourcesMain = sourceSets.main.get()
-        val contents = configurations.runtimeClasspath.get()
-            .map { if (it.isDirectory) it else zipTree(it) } +
-                sourcesMain.output
-        from(contents)
-    }
-    build {
-        dependsOn(packJar) // Trigger fat jar creation during build
+ktor {
+    fatJar {
+        archiveFileName.set("Nil3.jar")
     }
 }
- */
 
