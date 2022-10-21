68148acce9d5fa72550fb40520ce90fe90caddea
gemsvidø <gemsvido@gmail.com>
2022-10-20 22:33:51 -0700

    updating
    Signed-off-by: gemsvidø <gemsvido@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,13 +13,10 @@ repositories {
     mavenCentral()
 }
 
-repositories {
-    maven("https://repo.zugazagoitia.com/snapshots")
-}
 
 dependencies {
-    implementation("io.javalin", "javalin-ssl", "5.0.0-SNAPSHOT")
-    implementation("io.javalin", "javalin", "5.0.1")
+    implementation("io.javalin.community.ssl", "ssl-plugin", "5.1.1")
+    implementation("io.javalin", "javalin", "5.1.1")
     implementation("org.slf4j", "slf4j-simple", "2.0.1")
     implementation("org.junit.jupiter:junit-jupiter:5.9.0")
     testImplementation("org.jetbrains.kotlin:kotlin-test:1.7.10")
